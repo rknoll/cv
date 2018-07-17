d9370c84f10c17167ae62a6e829a2ae11f04e6fd
Richard Knoll <richard.knoll@bjss.com>
2018-07-17 22:53:26 +0100

    feat: page styling setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import style from './index.css';
+
+const width = 210;
+const height = 297;
+const outline = 20;
+
+const innerWidth = width - 2 * outline;
+const innerHeight = height - 2 * outline;
+
+const pages = 2;
 
 const Index = () => {
-  return <div>Richard Knoll</div>;
+  const content = (page) =>
+    <svg width={`${innerWidth}mm`} height={`${innerHeight}mm`}
+         viewBox={`0 ${innerHeight * page} ${innerWidth} ${innerHeight}`}>
+      <rect fill="white" stroke="#D3D3D3" x="0" y="0" width={innerWidth} height={innerHeight * 2} />
+      <text x="0" y="0">Richard Knoll</text>
+    </svg>;
+
+  return <div className={style.book}>
+    {[...Array(pages).keys()].map((page) => <div className={style.page} key={page}>
+      <div className={style.subpage}>
+        {content(page)}
+      </div>
+    </div>)}
+  </div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById('index'));
